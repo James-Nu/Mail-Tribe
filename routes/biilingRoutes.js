3c28a3acdb15ec43108c29202c5154408aa17809
James Nunoo <jamesnunoo@Jamess-MacBook-Pro.local>
2020-08-03 17:50:16 +0300

    api issues

--- routes/biilingRoutes.js
+++ routes/biilingRoutes.js
@@ -1,5 +1,7 @@
 const keys = require("../config/keys");
-const stripe = require("stripe")(keys.stripeSecretKey);
+const stripe = require("stripe")(
+  "sk_test_51HBjzDDOYWT7gjgiFFIy4WsDIg6h7EaB2Y4Hsp0Bf00NdUSyJD5qpyCqmxeHe3yGQ3ymAcdU8UNrYtKd1gNNr9p500LJiGnvcV"
+);
 const requireLogin = require("../middlewares/requireLogin");
 
 module.exports = (app) => {
